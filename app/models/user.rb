0e9c8037b32679eb258aa2dcac6bd5fbbb0518d4
seku <gamerrrr0911@gmail.com>
2025-12-04 11:08:57 +0900

    エラー表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,44 @@
 class User < ApplicationRecord
+  # Devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # ニックネーム
+  # nickname
   validates :nickname, presence: true
 
-  # 名前（全角）
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-
-  # 名前（カナ）
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-
-  # 生年月日
+  # birth date
   validates :birthday, presence: true
 
-  # パスワード（半角英数字混合）
+  # パスワード（英数字混合）
   validates :password,
             format: {
               with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
-              message: 'は半角英数字混合で入力してください'
+              message: "is invalid. Include both letters and numbers"
+            }
+
+  # 名前（全角）
+  validates :last_name, presence: true,
+            format: {
+              with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+              message: "is invalid. Input full-width characters"
+            }
+
+  validates :first_name, presence: true,
+            format: {
+              with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+              message: "is invalid. Input full-width characters"
+            }
+
+  # 名前（カナ）
+  validates :last_name_kana, presence: true,
+            format: {
+              with: /\A[ァ-ヶー－]+\z/,
+              message: "is invalid. Input full-width katakana characters"
+            }
+
+  validates :first_name_kana, presence: true,
+            format: {
+              with: /\A[ァ-ヶー－]+\z/,
+              message: "is invalid. Input full-width katakana characters"
             }
 end

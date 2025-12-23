6a0c96aab28bcd080d01f5fe9d700417d31ded4a
seku <gamerrrr0911@gmail.com>
2025-12-23 15:47:24 +0900

    一覧表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  # ActiveHash
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
@@ -10,12 +9,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_time
 
-  # 必須項目
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
 
-  # 価格
   validates :price,
             presence: true,
             numericality: {
@@ -24,7 +21,6 @@ class Item < ApplicationRecord
               less_than_or_equal_to: 9_999_999
             }
 
-  # ActiveHashの「---」を弾く
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }

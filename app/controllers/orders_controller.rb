1513711fa5ad843b19e303edbdeedfe0bb0568e0
seku <gamerrrr0911@gmail.com>
2026-02-17 11:05:58 +0900

    編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
   before_action :redirect_if_unbuyable
+  skip_before_action :verify_authenticity_token, only: :create
 
   def index
     @order_address = OrderAddress.new
